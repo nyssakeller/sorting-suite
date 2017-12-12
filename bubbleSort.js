a508b33259b8d522d3539a5d056bd9e69decc8f7
Nyssa Keller <nyssa1@gmail.com>
2017-12-12 08:59:38 -0700

    Make bubble sort function and start making tests for sorting small arrays

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,12 @@
-class bubbleSort {
-  constructor() {
+function bubbleSort(array) {
+  for(j = 0; j < array.length; j++) {
+    for(i = 0; i < array.length; i++) {
+      if(array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+      };
+    }; 
+  };
+};
 
-  }
-}
 
 module.exports = bubbleSort;
--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -5,7 +5,24 @@ const bubbleSort = require('../bubbleSort.js');
 describe('bubbleSort', function() {
 
   it('should be a function', function() {
-    assert.isFunction(bubbleSort, true);
-  })
+    assert.isFunction(bubbleSort);
+  });
 
-})
+  it('should be able to sort small arrays', function() {
+    let array = [8, 5];
+    bubbleSort(array);
+    expect(array).to.deep.equal([5, 8]);
+  });
+
+  it('should be able to sort an array of ten numbers', function() {
+    let array = [10, 5, 1, 9, 8, 2, 4, 7, 6, 3];
+    bubbleSort(array);
+    expect(array).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it('should be able to sort negative numbers', function() {
+    let array = [0, -1, -11, -4, -6, -12, -2];
+    bubbleSort(array);
+    expect(array).to.deep.equal([-12, -11, -6, -4, -2, -1, 0]);
+  })
+});
